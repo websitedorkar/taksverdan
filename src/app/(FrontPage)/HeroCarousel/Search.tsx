6015a7d4776d25faefea42b30b3c8a667ddc5234
Arif Hossin || Full Stack Developer <websitedorkar@gmail.com>
2024-05-02 23:29:08 +0600

    Update

--- src/app/(FrontPage)/HeroCarousel/Search.tsx
+++ src/app/(FrontPage)/HeroCarousel/Search.tsx
@@ -1,3 +1,151 @@
+
+// "use client";
+
+// import React, { useState } from "react";
+// import { Check, ChevronsUpDown } from "lucide-react";
+// import { Button } from "@/components/ui/button";
+// import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem } from "@/components/ui/command";
+// import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+// import { Input } from "@/components/ui/input";
+
+// export interface Framework {
+//   value: string;
+//   label: string;
+// }
+
+// export const frameworks: Framework[] = [
+//   {
+//     value: "next.js",
+//     label: "Next.js",
+//   },
+//   {
+//     value: "sveltekit",
+//     label: "SvelteKit",
+//   },
+//   {
+//     value: "nuxt.js",
+//     label: "Nuxt.js",
+//   },
+//   {
+//     value: "remix",
+//     label: "Remix",
+//   },
+//   {
+//     value: "astro",
+//     label: "Astro",
+//   },
+// ];
+
+// const Search: React.FC = () => {
+//     const [open, setOpen] = useState<boolean>(false);
+//     const [value, setValue] = useState<string>('');
+
+//   return (
+//     // <Popover open={open} onOpenChange={setOpen}>
+//     //   <PopoverTrigger asChild>
+//     //     <Button
+//     //       variant="outline"
+//     //       role="combobox"
+//     //       aria-expanded={open}
+//     //       className="w-[200px] justify-between"
+//     //     >
+//     //       {value
+//     //         ? frameworks.find((framework) => framework.value === value)?.label
+//     //         : "Select framework..."}
+//     //       <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+//     //     </Button>
+//     //   </PopoverTrigger>
+//     //   <PopoverContent className="w-[200px] p-0">
+//     //     {[ // Enclose the children within an array
+//     //       <Command key="command">
+//     //         <CommandInput placeholder="Search framework..." />
+//     //         <CommandEmpty>No framework found.</CommandEmpty>
+//     //         <CommandGroup>
+//     //           {frameworks.map((framework) => (
+//     //             <CommandItem
+//     //               key={framework.value}
+//     //               value={framework.value}
+//     //               onSelect={(currentValue) => {
+//     //                 setValue(currentValue === value ? "" : currentValue);
+//     //                 setOpen(false);
+//     //               }}
+//     //             >
+//     //               <Check
+//     //                 className={
+//     //                   value === framework.value
+//     //                     ? "mr-2 h-4 w-4 opacity-100"
+//     //                     : "mr-2 h-4 w-4 opacity-0"
+//     //                 }
+//     //               />
+//     //               { framework.label }
+//     //             </CommandItem>
+//     //           ))}
+//     //         </CommandGroup>
+//     //       </Command>
+//     //     ]} // Close the array
+//     //   </PopoverContent>
+
+//     // </Popover>
+    
+//     <Popover>
+//       <PopoverTrigger asChild>
+//         <Button variant="outline">Open popover</Button>
+//       </PopoverTrigger>
+//       <PopoverContent className="w-80">
+//         <div className="grid gap-4">
+//           <div className="space-y-2">
+//             <h4 className="font-medium leading-none">Dimensions</h4>
+//             <p className="text-sm text-muted-foreground">
+//               Set the dimensions for the layer.
+//             </p>
+//           </div>
+//           <div className="grid gap-2">
+//             <div className="grid grid-cols-3 items-center gap-4">
+//               <label htmlFor="width">Width</label>
+//               <Input
+//                 id="width"
+//                 defaultValue="100%"
+//                 className="col-span-2 h-8"
+//               />
+//             </div>
+//             <div className="grid grid-cols-3 items-center gap-4">
+//               <label htmlFor="maxWidth">Max. width</label>
+//               <Input
+//                 id="maxWidth"
+//                 defaultValue="300px"
+//                 className="col-span-2 h-8"
+//               />
+//             </div>
+//             <div className="grid grid-cols-3 items-center gap-4">
+//               <label htmlFor="height">Height</label>
+//               <Input
+//                 id="height"
+//                 defaultValue="25px"
+//                 className="col-span-2 h-8"
+//               />
+//             </div>
+//             <div className="grid grid-cols-3 items-center gap-4">
+//               <label htmlFor="maxHeight">Max. height</label>
+//               <Input
+//                 id="maxHeight"
+//                 defaultValue="none"
+//                 className="col-span-2 h-8"
+//               />
+//             </div>
+//           </div>
+//         </div>
+//       </PopoverContent>
+//     </Popover>
+
+//   );
+// };
+
+// export default Search;
+
+
+
+
+
 "use client";
 
 import React, { useEffect, useState } from 'react';
@@ -5,28 +153,23 @@ import Search_SVG from '@/images/icon-search.svg';
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 
-import {
-    Calculator,
-    Calendar,
-    CreditCard,
-    Settings,
-    Smile,
-    User,
-  } from "lucide-react"
+import { Check, ChevronsUpDown } from "lucide-react";
 
 import {
-    Command,
-    CommandDialog,
-    CommandEmpty,
-    CommandGroup,
-    CommandInput,
-    CommandItem,
-    CommandList,
-    CommandSeparator,
-    CommandShortcut,
-  } from "@/components/ui/command"
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command"
 
-  
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { Input } from "@/components/ui/input";
+import { Label } from '@/components/ui/label';
+ 
 
 const Search = () => {
     const [searchQuery, setSearchQuery] = useState<string>('');
@@ -47,59 +190,7 @@ const Search = () => {
     <div className='bg-white text-dark p-[6px] rounded-full mb-3'>
         <div className="grid grid-cols-[1fr_1fr_60px] items-center">
             <div>Lorem ipsum dolor sit amet.</div>
-            <div>
-                
-                
-                <input
-                    type='text'
-                    onFocus={() => setOpen(true)}
-                    onChange={(e) => setSearchQuery(e.target.value)}
-                    className='border-none shadow-none outline-none bg-transparent w-full lg:w-auto lg:min-w-[140px] lg:max-w-[140px] transition-all ease-linear duration-200 focus:lg:min-w-[200px] focus:lg:max-w-[200px]'
-                    placeholder='Search course...'
-                />
-                        
-                <CommandDialog open={open} onOpenChange={setOpen}>
-                    <CommandInput placeholder="Type a command or search..." />
-                    <CommandList>
-                    <CommandEmpty>No results found.</CommandEmpty>
-                    <CommandGroup heading="Suggestions">
-                        <CommandItem>
-                        <Calendar className="mr-2 h-4 w-4" />
-                        <span>Calendar</span>
-                        </CommandItem>
-                        <CommandItem>
-                        <Smile className="mr-2 h-4 w-4" />
-                        <span>Search Emoji</span>
-                        </CommandItem>
-                        <CommandItem>
-                        <Calculator className="mr-2 h-4 w-4" />
-                        <span>Calculator</span>
-                        </CommandItem>
-                    </CommandGroup>
-                    <CommandSeparator />
-                    <CommandGroup heading="Settings">
-                        <CommandItem>
-                        <User className="mr-2 h-4 w-4" />
-                        <span>Profile</span>
-                        <CommandShortcut>⌘P</CommandShortcut>
-                        </CommandItem>
-                        <CommandItem>
-                        <CreditCard className="mr-2 h-4 w-4" />
-                        <span>Billing</span>
-                        <CommandShortcut>⌘B</CommandShortcut>
-                        </CommandItem>
-                        <CommandItem>
-                        <Settings className="mr-2 h-4 w-4" />
-                        <span>Settings</span>
-                        <CommandShortcut>⌘S</CommandShortcut>
-                        </CommandItem>
-                    </CommandGroup>
-                    </CommandList>
-                </CommandDialog>
-
-
-        
-                </div>
+            <div><SearchTour /></div>
             <div className='inline-flex items-center'>
                 <Button variant={'secondary'} size={'default'} className='p-0 h-auto'>
                     <Image src={Search_SVG} alt='Search Now' className='w-[58px] h-[58px]'/>
@@ -111,3 +202,91 @@ const Search = () => {
 }
 
 export default Search;
+
+
+
+const SearchTour: React.FC = () => {
+  const [open, setOpen] = useState<boolean>(false);
+  const [value, setValue] = useState<string>('');
+
+  interface Framework {
+    value: string;
+    label: string;
+  }
+
+  const frameworks: Framework[] = [
+    {
+      value: "next.js",
+      label: "Next.js",
+    },
+    {
+      value: "sveltekit",
+      label: "SvelteKit",
+    },
+    {
+      value: "nuxt.js",
+      label: "Nuxt.js",
+    },
+    {
+      value: "remix",
+      label: "Remix",
+    },
+    {
+      value: "astro",
+      label: "Astro",
+    },
+  ]
+
+  return (
+    <div>
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between"
+        >
+        {value
+          ? frameworks.find((framework) => framework.value === value)?.label
+          : "Select tour..."}
+        </Button>
+        {/* <Label htmlFor="form-label--tour">Tour</Label> */}
+        {/* <Label className='block text-[16px] text-dark'>Tour</Label> */}
+        {/* <Input
+            type='text'
+            id='form-label--tour'
+            onFocus={() => setOpen(true)}
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            className='border-none shadow-none outline-none bg-transparent text-[18px] font-roboto placeholder:text-dark/50'
+            placeholder='Choose Tour'
+        /> */}
+
+      </PopoverTrigger>
+      <PopoverContent className="w-80 p-0 shadow-none border-none bg-transparent">
+        <Command className="rounded-lg border shadow-md">
+          <CommandInput placeholder="Type a command or search..." />
+          <CommandList>
+            <CommandEmpty>No results found.</CommandEmpty>
+            <CommandGroup>
+            {frameworks.map((framework) => (
+              <CommandItem
+                key={framework.value}
+                value={framework.value}
+                onSelect={(currentValue) => {
+                  setValue(currentValue === value ? "" : currentValue)
+                  setOpen(false)
+                }}
+              >
+                {framework.label}
+              </CommandItem>
+            ))}
+          </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+    </div>
+  )
+}
--- src/app/(FrontPage)/HeroCarousel/index.tsx
+++ src/app/(FrontPage)/HeroCarousel/index.tsx
@@ -22,9 +22,9 @@ const HeroCarousel = () => {
     return (
     <div className='p-4'>
 
-        <div className="relative rounded-xl overflow-hidden bg-slate-100">
+        <div className="relative rounded-[1.75rem] overflow-hidden bg-slate-100">
             
-            <div className="absolute top-1/2 start-0 end-0 z-10 w-full flex items-center gap-[8px] justify-between px-4">
+            <div className="absolute top-1/2 start-0 end-0 z-[20] w-full flex items-center gap-[8px] justify-between px-4">
                 {/* Arrow Left */}
                 <span className="hero-carousel--prev inline-flex items-center justify-center w-[40px] min-w-[40px] h-[40px] cursor-pointer">
                     <Image src={ArrowLeft} alt='arrow-left' />
@@ -50,14 +50,14 @@ const HeroCarousel = () => {
                 {slides.map(slide => {
                     return (
                         <SwiperSlide key={slide.id}>
-                            <div className="bg-slate-100 flex flex-col justify-end bg-[bottom_center] bg-cover px-4 py-[30px] text-center min-h-[660px]" style={{ backgroundImage: `url(${slide.thumb.src})` }}></div>
+                            <div className="bg-slate-100 flex flex-col justify-end bg-[bottom_center] bg-cover px-4 py-[30px] text-center min-h-[660px] relative before:content-[''] before:absolute before:bg-black/20 before:w-full before:h-full before:z-[1] before:start-0 before:top-0" style={{ backgroundImage: `url(${slide.thumb.src})` }}></div>
                         </SwiperSlide>
                     )
                 })}
             </Swiper>
             }
 
-            <div className="min-h-[660px] absolute z-20 top-0 start-0 end-0 flex flex-col justify-end p-[40px] text-white gap-4">
+            <div className="min-h-[660px] absolute z-[12] top-0 start-0 end-0 flex flex-col justify-end p-[40px] text-white gap-4">
                 <div className="container max-w-[680px]">
                     <h2 className="font-poppins font-semibold text-[61px] text-center leading-8 mb-3">Explore more for less</h2>
                     <div className="">
