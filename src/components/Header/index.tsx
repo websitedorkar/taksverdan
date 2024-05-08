638ee32c7a9aa9469ced104c82b859e8ed50a1a2
Arif Hossin || Full Stack Developer <websitedorkar@gmail.com>
2024-05-08 23:03:22 +0600

    Update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,33 +6,33 @@ import LOGO_SM from './img/logo-sm.svg';
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 import Navbar from './navbar';
-import { useMediaQuery } from '@/hooks/use-media-query';
 import Toggler from './Toggler';
 import Link from 'next/link';
 
 const Header = () => {
-    // Check if window is defined before accessing its properties
-    const isDesktop = useMediaQuery("(min-width: 1199px)");
-    const isMobile = useMediaQuery("(max-width: 768px)");
 
   return (
     <header className='absolute top-0 start-0 end-0 z-20 px-[35px] lg:px-[60px] py-[40px]'>
         <div className="flex gap-4 items-center justify-between">
             {/* LOGO */}
             <div className="header__logo">
-                <Image src={isDesktop ? LOGO_DESKTOP : LOGO_SM} alt='logo' />
+                <span className="inline-flex xl:hidden">
+                    <Image src={LOGO_SM} alt='logo' />
+                </span>
+                <span className="hidden xl:inline-flex">
+                    <Image src={LOGO_DESKTOP} alt='logo' />
+                </span>
             </div>
-            {isDesktop &&
-            <div className="header__menu">
+            <div className="header__menu hidden xl:block">
                 <Navbar />
             </div>        
-            }
-            <div className="header__action">
-            {isDesktop ?
-                <Link href={'#map-destination-section'}><Button variant={'secondary'} size={'sm'}>MAP</Button></Link>
-                :
-                <Toggler />
-            }
+            <div className="header__action inline-flex">
+                <div className="hidden xl:block">
+                    <Link href={'#map-destination-section'}><Button variant={'secondary'} size={'sm'}>MAP</Button></Link>
+                </div>
+                <div className="inline-flex xl:hidden">
+                    <Toggler />
+                </div>
             </div>
         </div>
     </header>
