4fa9168ce76e54360740c3d8d6c84a7f26f9e0a5
Arif Hossin || Full Stack Developer <websitedorkar@gmail.com>
2024-05-12 15:52:50 +0600

    Update

--- src/components/PrimaryHeader/data.tsx
+++ src/components/PrimaryHeader/data.tsx
@@ -1,4 +1,8 @@
 import Flag_1 from './img/1.png';
+import Banner__1 from './img/banner-1.png';
+import Banner__2 from './img/banner-2.png';
+import Banner__3 from './img/banner-3.jpg';
+import Banner__4 from './img/banner-4.jpg';
 
 interface SubmenuItem {
   id: number;
@@ -159,4 +163,56 @@ const menus: MenusJSON[] = [
   },
 ];
 
-export { menus };
+interface MapJSON {
+  id: number;
+  slug?: string;
+  thumbnail?: any;
+  title: string;
+  location: string;
+}
+
+const maps: MapJSON[] = [
+  {
+    id: 1,
+    slug: 'lake-house-pier',
+    thumbnail: Banner__1,
+    title: "Lakehouse Pier",
+    location: 'India, Bali',
+  },
+  {
+    id: 2,
+    thumbnail: Banner__2,
+    slug: 'lake-house-pier',
+    title: "Lakehouse Pier",
+    location: 'India, Bali',
+  },
+  {
+    id: 3,
+    thumbnail: Banner__3,
+    slug: 'lake-house-pier',
+    title: "Lakehouse Pier",
+    location: 'India, Bali',
+  },
+  {
+    id: 4,
+    thumbnail: Banner__4,
+    slug: 'lake-house-pier',
+    title: "Lakehouse Pier",
+    location: 'India, Bali',
+  },
+  {
+    id: 5,
+    thumbnail: Banner__1,
+    slug: 'lake-house-pier',
+    title: "Lakehouse Pier",
+    location: 'India, Bali',
+  },
+  {
+    id: 6,
+    thumbnail: Banner__2,
+    slug: 'lake-house-pier',
+    title: "Lakehouse Pier",
+    location: 'India, Bali',
+  },
+]
+export { menus, maps };
--- src/components/PrimaryHeader/index.tsx
+++ src/components/PrimaryHeader/index.tsx
@@ -8,40 +8,45 @@ import { Button } from '@/components/ui/button';
 import Navbar from './navbar';
 import Toggler from './Toggler';
 import Link from 'next/link';
+import MapButton from './MapButton';
 
 interface PrimaryHeaderProps {
     btn_label: string;
     btn_url: string;
+    isMap?: boolean;
   }
   
-const PrimaryHeader: React.FC<PrimaryHeaderProps> = ({ btn_label, btn_url }) => {
+const PrimaryHeader: React.FC<PrimaryHeaderProps> = ({ btn_label, btn_url, isMap }) => {
   return (
-    <header className='py-[10px] border-b border-[#E5E5E5] z-20 relative'>
-        <div className="container-fluid">
-            <div className="flex gap-4 items-center justify-between">
-                {/* LOGO */}
-                <Link href={'/'} className="header__logo inline-flex">
-                    <span className="inline-flex xl:hidden">
-                        <Image src={LOGO_SM} alt='logo' />
-                    </span>
-                    <span className="hidden xl:inline-flex">
-                        <Image src={LOGO_DESKTOP} alt='logo'  className='h-[56px]'/>
-                    </span>
-                </Link>
-                <div className="header__menu hidden xl:block">
-                    <Navbar />
-                </div>        
-                <div className="header__action inline-flex">
-                    <div className="hidden xl:block">
-                        <Link href={btn_url}><Button variant={'default'} size={'sm'} className='px-5'>{btn_label}</Button></Link>
-                    </div>
-                    <div className="inline-flex xl:hidden">
-                        <Toggler />
+    <>
+        <header className='py-[10px] border-b border-[#E5E5E5] z-20 relative'>
+            <div className="container-fluid">
+                <div className="flex gap-4 items-center justify-between">
+                    {/* LOGO */}
+                    <Link href={'/'} className="header__logo inline-flex">
+                        <span className="inline-flex xl:hidden">
+                            <Image src={LOGO_SM} alt='logo' />
+                        </span>
+                        <span className="hidden xl:inline-flex">
+                            <Image src={LOGO_DESKTOP} alt='logo'  className='h-[56px]'/>
+                        </span>
+                    </Link>
+                    <div className="header__menu hidden xl:block">
+                        <Navbar />
+                    </div>        
+                    <div className="header__action inline-flex">
+                        <div className="hidden xl:block">
+                            {/* <Link href={btn_url}><Button variant={'default'} size={'sm'} className='px-5'>{btn_label}</Button></Link> */}
+                            <MapButton />
+                        </div>
+                        <div className="inline-flex xl:hidden">
+                            <Toggler />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </header>
+        </header>
+        </>
   )
 }
 
